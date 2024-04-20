3c9aa955e3d5546a73acd2306bc49cc2180cd1a6
Bhuvaneshwarisharanappa <161186117+Bhuvaneshwarisharanappa@users.noreply.github.com>
2024-04-20 20:55:45 +0530

    Create Week#2 Mobile phone.java

---
+++ Week#2 Mobile phone.java
@@ -0,0 +1,52 @@
+package week2
+public class MobilePhone 
+{
+    private String brand;
+    private String model;
+    private double price;
+    public MobilePhone(String brand, String model, double price) 
+    {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+    public String getBrand() 
+    {
+        return brand;
+    }
+    public void setBrand(String brand) 
+    {
+        this.brand = brand;
+    }
+    public String getModel()
+    {
+        return model;
+    }
+    public void setModel(String model) 
+    {
+        this.model = model;
+    }
+    public double getPrice() 
+    {
+        return price;
+    }
+    public void setPrice(double price) 
+    {
+        this.price = price;
+    }
+    public void printDetails()
+    {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: $" + price);
+    }
+    public static void main(String[] args) 
+    {
+        MobilePhone myPhone = new MobilePhone("Samsung", "Galaxy S20", 999.9)
+        myPhone.printDetails();
+        myPhone.setPrice(899.99);
+        myPhone.setModel("Galaxy S21");
+        System.out.println("\nAfter updating details:");
+        myPhone.printDetails();
+    }
+}
